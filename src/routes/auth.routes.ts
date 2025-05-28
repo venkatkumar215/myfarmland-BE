3dd1f4650e10951f1fb6bdd12ea9bb05f0e2ba20
venkat2151993 <167997295+venkat2151993@users.noreply.github.com>
2025-05-28 13:33:41 +0200

    request otp service

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,7 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { requestOtp } from "../controllers/otp.controller.ts/requestOtp.controller";
 
 const router = express.Router();
 
-router.post("/request-otp", requestOtp); // /api/auth/request-otp
-
+router.post("/request-otp", requestOtp);
 export default router;
-//
