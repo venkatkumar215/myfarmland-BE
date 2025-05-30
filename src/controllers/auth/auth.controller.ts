8f172c6e316c41140799c2f8f4e1593a55cfa295
venkat2151993 <167997295+venkat2151993@users.noreply.github.com>
2025-05-31 00:10:07 +0200

    remove sceret

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,1 +1,31 @@
-import express from "express";
+// authMiddleware.ts
+import { Request, Response, NextFunction } from "express";
+import admin from "../../config/firebase/firebase";
+import { ERRORS } from "../../config/constants/error";
+
+export interface AuthRequest extends Request {
+  user?: admin.auth.DecodedIdToken;
+}
+
+export const verifyFirebaseToken = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(ERRORS.UNAUTHOZIRED_TOKEN);
+  }
+
+  const idToken = authHeader.split("Bearer ")[1];
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken; // Attach decoded token (user info) to request object
+    next();
+  } catch (error) {
+    console.error("Error verifying Firebase ID token:", error);
+    return next(ERRORS.INVALID_TOKEN);
+  }
+};
